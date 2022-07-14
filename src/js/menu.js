48e7c45becca139230583064300a5ba541795cd2
Tasha1003Natasha <0955712824n@gmail.com>
2022-07-14 13:32:34 +0300

    Проект

---
+++ src/js/menu.js
@@ -0,0 +1,27 @@
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu-mobile]');
+  const tabletMenuRef = document.querySelector('[data-menu-tablet]');
+  const body = document.querySelector('body');
+  const logo = document.querySelector('[data-logo]');
+  const navLinks = document.querySelectorAll('.navigation__link');
+
+  menuBtnRef.addEventListener('click', toogleMenu);
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', toogleMenu);
+  });
+
+  function toogleMenu() {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+    tabletMenuRef.classList.toggle('is-open');
+
+    body.classList.toggle('no-scroll');
+    logo.classList.toggle('logo-light');
+  }
+})();
